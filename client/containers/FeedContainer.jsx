1bed82565083257c4da4c05077261317a9a0600a
Dakota Gilbreath <46034431+dgilbrea92@users.noreply.github.com>
2020-06-09 12:39:55 -0700

    Finish merging FeedContainer

--- client/containers/FeedContainer.jsx
+++ client/containers/FeedContainer.jsx
@@ -9,28 +9,16 @@
  * ************************************
  */
 
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as ticketActions from "../actions/ticketActions";
-import MenteeTicketBox from "../components/MenteeTicketBox";
-import BystanderTicketBox from "../components/BystanderTicketBox";
-import TicketCreator from "../components/TicketCreator";
-=======
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions/ticketActions';
-
 // import components
 // import MenteeTicketBox from '../components/MenteeTicketBox';
 // import BystanderTicketBox from '../components/BystanderTicketBox';
 import TicketCreator from '../components/TicketCreator';
 import ResolveModal from '../components/ResolveModal';
 import TicketStream from './TicketStream';
-
->>>>>>> 63b8510e8c0363e51a49777de5f19a7555a84686
 
 const mapStateToProps = state => ({
   messageInput: state.tickets.messageInput,
@@ -44,7 +32,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(ticketActions, dispatch);
+  bindActionCreators(actions, dispatch);
 
 class FeedContainer extends Component {
   constructor(props) {
